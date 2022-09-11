d8d3b2c3d7fc4cf3093fa51722a9b9ab749801fd
Ryan Williams <ryan@runsascoded.com>
2022-09-11 19:12:06 -0400

    nextra checkpoint, headings fixed

--- theme.config.js
+++ theme.config.js
@@ -1,26 +1,35 @@
 export default {
-    projectLink: 'https://github.com/shuding/nextra', // GitHub link in the navbar
-    docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master', // base URL for the docs repository
-    titleSuffix: ' – Nextra',
-    nextLinks: true,
-    prevLinks: true,
-    search: true,
-    customSearch: null, // customizable, you can use algolia for example
-    darkMode: true,
-    footer: true,
-    footerText: `MIT ${new Date().getFullYear()} © Shu Ding.`,
-    footerEditLink: `Edit this page on GitHub`,
+    project: { link: 'https://github.com/neighbor-ryan/ebikes', }, // GitHub link in the navbar
+    docsRepositoryBase: 'https://github.com/neighbor-ryan/ebikes', // base URL for the docs repository
+    titleSuffix: ' - E-bikes are a Transportation Revolution',
+    // defaultMenuCollapsed: true,
+    toc: {
+        float: true,
+    },
+    floatTOC: true,
+    navigation: {
+        next: true,
+        prev: true,
+    },
+    // nextLinks: true,
+    // prevLinks: true,
+    // search: true,
+    // customSearch: null, // customizable, you can use algolia for example
+    // darkMode: true,
+    // footer: true,
+    // footerText: '',  // `MIT ${new Date().getFullYear()} © Shu Ding.`,
+    editLink: { text: `Edit this page on GitHub`, },
     logo: (
         <>
-            <svg>...</svg>
-            <span>Next.js Static Site Generator</span>
+            <img src={"favicon.ico"} />
+            <span>E-bikes are a Transportation Revolution</span>
         </>
     ),
     head: (
         <>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta name="description" content="Nextra: the next docs builder" />
-            <meta name="og:title" content="Nextra: the next docs builder" />
+            {/*<meta name="description" content="" />*/}
+            <meta name="og:title" content="E-bikes are a Transportation Revolution" />
         </>
     ),
 }
