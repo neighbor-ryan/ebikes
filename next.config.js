e5b114cdc873a47c6422f230f881908f3f55e99e
Ryan Williams <ryan@runsascoded.com>
2022-09-11 20:53:57 -0400

    break up `bikes` into multiple pages

--- next.config.js
+++ next.config.js
@@ -5,5 +5,6 @@ const withNextra = require('nextra')({
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
   reactStrictMode: true,
   swcMinify: true,
+  //trailingSlash: true,
 })
 module.exports = withNextra()
